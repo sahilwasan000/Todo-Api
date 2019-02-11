7f4b63a96d5acc13e4023f84fe861f6e2d02142f
Sahil Wasan <32892440+sahilwasan000@users.noreply.github.com>
2019-02-11 10:35:33 +0530

    Add GET /todos

--- server/server.js
+++ server/server.js
@@ -21,6 +21,14 @@
     });
   });
 
+  app.get('/todos', (req, res) => {
+    Todo.find().then((todos) => {
+      res.send({todos});
+    }, (e) => {
+      res.status(400).send(e);
+    })
+  });
+
   app.listen(8080, ()=> {
     console.log('App listening on port 8080');
   });
