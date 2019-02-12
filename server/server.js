8d0a19b369f1f8160d82ac515bec0922174ab1b0
Sahil Wasan <32892440+sahilwasan000@users.noreply.github.com>
2019-02-12 10:52:26 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
   var express = require('express');
   var bodyParser = require('body-parser');
+  const {ObjectID} = require('mongodb'); //for requiring the id
 
   var {mongoose} = require('./db/mongoose');
   var {Todo} = require('./models/todo');
@@ -27,6 +28,29 @@
     }, (e) => {
       res.status(400).send(e);
     })
+  });
+
+  app.get('/todos/:id', (req,res) => {
+    var id = req.params.id;
+
+    //res.send(req.params); -> returns key-value pairs for id:data;
+    // validate id
+    //send 404, if not valid
+    if(!ObjectID.isValid(id)){
+      return res.status(404).send();
+    }
+
+    Todo.findById(id).then((todo) => {
+      if(!todo) {
+        return res.status(404).send();
+      }
+
+      res.send({todo});
+    }).catch((e) => {
+      res.status(404).send();
+    });
+
+
   });
 
   app.listen(8080, ()=> {
