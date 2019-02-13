571aab6f10bbf912eebcf4be0cb7d4b974d06b33
Sahil Wasan <sahilwasan000@gmail.com>
2019-02-13 18:05:51 +0530

    Add Test for todos

--- server/server.js
+++ server/server.js
@@ -1,72 +1,108 @@
-  var express = require('express');
-  var bodyParser = require('body-parser');
-  const {ObjectID} = require('mongodb'); //for requiring the id
+const expect = require('expect');
+const request = require('supertest');
+const {ObjectID} = require('mongodb');
 
-  var {mongoose} = require('./db/mongoose');
-  var {Todo} = require('./models/todo');
-  var {user} = require('./models/user');
+const {app} = require('./../server.js');
+const {Todo} = require('./../models/todo');
 
-  var app =  express();
+const todos = [{
+  _id: new ObjectID(),
+  text: 'Meet me in Galaxy 3.5'
+},{
+  _id: new ObjectID(),
+  text: 'Please come fast'
+}];
 
-  app.use(bodyParser.json());
+// beforeEach((done) => {
+//   Todo.remove({}).then(() => done());
+// }); //to empty the db to run the test, as length is equated with 1 at 28.
 
-  app.post('/todos', (req,res) => {
-    var todo = new Todo({
-      text: req.body.text
+beforeEach((done) => {
+  Todo.remove({}).then(() => {
+    return Todo.insertMany(todos);
+  }).then(() => done());
+});
+
+describe('POST /todos', () => { //(type of req Document)
+    it('should create a todo item', (done) => { //describe test
+      var text = 'New Text Created.';
+
+      request(app)//document
+      .post('/todos')
+      .send({text})//data to be send
+      .expect(200)//status
+      .expect((res) => {
+        expect(res.body.text).toBe(text);//check if same data is returned
+      })
+      .end((err, res) => {
+        if (err){
+          return done(err); //terminates the fx here only
+        }
+
+        Todo.find({text}).then((todos) => {
+          expect(todos.length).toBe(1);
+          expect(todos[0].text).toBe(text);
+          done();
+        }).catch((e) => done(e));
+      });
     });
 
-    todo.save().then((doc) => {
-      res.send(doc);
-    }, (e) => {
-      res.status(400).send(e);
+    it('should not create a todo item', (done) => {
+      request(app)
+      .post('/todos')
+      .send({})
+      .expect(400)
+      .end((err, res) => {
+        if (err){
+          return done(err);
+        }
+
+        Todo.find().then((todos) => {
+          expect(todos.length).toBe(2);
+          done();
+        }).catch((e) => done(e));
+      });
     });
+});
+
+describe('GET /todos', () => {
+  it('should get all todos', (done) => {
+    request(app)
+      .get('/todos')
+      .expect(200)
+      .expect((res) =>{
+        expect(res.body.todos.length).toBe(2);
+      })
+      .end(done);
+  });
+});
+
+
+describe('GET /todos/:id', () => {
+  it('should return todo docs', (done) => {
+    request(app)
+      .get(`/todos/${todos[0]._id.toHexString()}`)//toHexString() converts obj to string
+      .expect(200)
+      .expect((res) => {
+        expect(res.body.todo.text).toBe(todos[0].text);
+      })
+      .end(done);
   });
 
-  app.get('/todos', (req, res) => {
-    Todo.find().then((todos) => {
-      res.send({todos});
-    }, (e) => {
-      res.status(400).send(e);
-    })
+  it('should return 404 if todo isnt found', (done) => {
+
+    var hexId = new ObjectID().toHexString();
+
+    request(app)
+    .get(`/todos/${hexId}`)
+    .expect(404)
+    .end(done);
   });
 
-  app.get('/todos/:id', (req,res) => {
-    var id = req.params.id;
-
-    //res.send(req.params); -> returns key-value pairs for id:data;
-    // validate id
-    //send 404, if not valid
-    if(!ObjectID.isValid(id)){
-      return res.status(404).send();
-    }
-
-    Todo.findById(id).then((todo) => {
-      if(!todo) {
-        return res.status(404).send();
-      }
-
-      res.send({todo});
-    }).catch((e) => {
-      res.status(404).send();
-    });
-
-
+  it('should return 404 for non-object ids', (done) => {
+      request(app)
+      .get('/todos/123')
+      .expect(404)
+      .end(done);
   });
-
-  app.listen(8080, ()=> {
-    console.log('App listening on port 8080');
-  });
-
-  module.exports = {app};
-
-
-  // var newTodo = new Todo({
-  //   text: 'We are just walking different paths.'
-  // });
-  //
-  //
-  // newTodo.save().then((doc) => {
-  //   console.log('Saved Documents', doc);
-  // }, (e) => {
-  //   console.log('unable to save');
-  // });
+});
