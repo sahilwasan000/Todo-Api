892681fc3c131808d697ed9c864159624c88be87
Sahil Wasan <sahilwasan000@gmail.com>
2019-02-14 11:17:02 +0530

    Update server.test.js

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -1,12 +1,15 @@
 const expect = require('expect');
 const request = require('supertest');
+const {ObjectID} = require('mongodb');
 
 const {app} = require('./../server.js');
 const {Todo} = require('./../models/todo');
 
 const todos = [{
+  _id: new ObjectID(),
   text: 'Meet me in Galaxy 3.5'
 },{
+  _id: new ObjectID(),
   text: 'Please come fast'
 }];
 
@@ -73,3 +76,74 @@ describe('GET /todos', () => {
       .end(done);
   });
 });
+
+
+describe('GET /todos/:id', () => {
+  it('should return todo docs', (done) => {
+    request(app)
+      .get(`/todos/${todos[0]._id.toHexString()}`)//toHexString() converts obj to string
+      .expect(200)
+      .expect((res) => {
+        expect(res.body.todo.text).toBe(todos[0].text);
+      })
+      .end(done);
+  });
+
+  it('should return 404 if todo isnt found', (done) => {
+
+    var hexId = new ObjectID().toHexString();
+
+    request(app)
+    .get(`/todos/${hexId}`)
+    .expect(404)
+    .end(done);
+  });
+
+  it('should return 404 for non-object ids', (done) => {
+      request(app)
+      .get('/todos/123')
+      .expect(404)
+      .end(done);
+  });
+});
+
+describe('DELETE /todos/:id', () => {
+  it('should remove a todo' , (done) => {
+    var hexId = todos[1]._id.toHexString();
+
+    request(app)
+    .delete(`/todos/${hexId}`)
+    .expect(200)
+    .expect((res) => {
+      expect(res.body.todo._id).toBe(hexId);
+    })
+    .end((err, res) => {
+      if(err){
+        return done(err);
+      }
+
+    Todo.findById(hexId).then((todo) => {
+      expect(todo).toNotExist();
+      done();
+    }).catch((e) => done(e));
+    });
+  });
+
+  it('should return 404 if todo isnt found', (done) => {
+    var hexId = new ObjectID().toHexString();
+
+    request(app)
+    .get(`/todos/${hexId}`)
+    .expect(404)
+    .end(done);
+  });
+
+  it('should return 404 if objectID isnt valid', (done) => {
+    var hexId = new ObjectID().toHexString();
+
+    request(app)
+    .get('/todos/123')
+    .expect(404)
+    .end(done);
+    });
+  });
