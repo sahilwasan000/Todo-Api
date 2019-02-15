08c15ae8a8450a202b7d567abee7fff87210a6f5
Sahil Wasan <sahilwasan000@gmail.com>
2019-02-15 21:21:58 +0530

    Add Update Method

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
-  var express = require('express');
-  var bodyParser = require('body-parser');
+  const _ = require('lodash');
+  const express = require('express');
+  const bodyParser = require('body-parser');
   const {ObjectID} = require('mongodb'); //for requiring the id
 
   var {mongoose} = require('./db/mongoose');
@@ -71,6 +72,25 @@
       });
     });
 });
+
+ app.patch('/todos/:id', (req,res) => {
+   var id = req.params.id;
+   var body = _.pick(req.body, ['text']);//pick method to select only those properties which may be edited by the user.
+
+   if(!ObjectID.isValid(id)) {
+     return res.status(404).send();
+   }
+
+   Todo.findByIdAndUpdate(id, {$set:body}, {new: true}).then((todo) => {
+     if(!todo){
+       return res.status(404).send();
+     }
+
+     res.send({todo});
+   }).catch((e) => {
+     res.status(404).send();
+   })
+ });
 
   app.listen(port, ()=> {
     console.log(`App listening on port ${port}`);
