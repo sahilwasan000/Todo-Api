4e3efb6f34e210e2f3275d8475951353f1f3e12d
Sahil Wasan <sahilwasan000@gmail.com>
2019-03-06 11:02:44 +0530

    Add Security

--- server/models/user.js
+++ server/models/user.js
@@ -1,9 +1,10 @@
 
 var mongoose = require('mongoose');
 const validator = require('validator');
+const jwt = require('jsonwebtoken');
+const _ = require('lodash');
 
-
-var User = mongoose.model('User', {
+var UserSchema = new mongoose.Schema({
   email:{
     type: String,
     required: true,
@@ -31,5 +32,26 @@ var User = mongoose.model('User', {
     }
   }]
 });
+
+UserSchema.methods.toJSON = function () {
+  var user = this;
+  var userObject = user.toObject();
+
+  return _.pick(userObject, ['_id', 'email']);
+};//function responsible for displaying only those properties which are necessary for user to see.
+
+UserSchema.methods.generateAuthToken = function() {  //UserSchema is an obj.
+  var user = this;
+  var access = 'auth';
+  var token = jwt.sign({_id: user._id.toHexString(),access}, 'abcabc').toString();
+
+  user.tokens = user.tokens.concat([{access, token}]);
+
+  return user.save().then(() => {
+    return token;
+  });
+};
+
+var User = mongoose.model('User', UserSchema);
 
 module.exports = {User};
