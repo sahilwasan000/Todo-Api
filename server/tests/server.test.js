de6d377bdc3e712ad5dd3e73a1693da8eceed159
Sahil Wasan <sahilwasan000@gmail.com>
2019-03-10 16:42:22 +0530

    Add

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -2,26 +2,17 @@ const expect = require('expect');
 const request = require('supertest');
 const {ObjectID} = require('mongodb');
 
-const {app} = require('./../server.js');
+const {app} = require('./../server');
 const {Todo} = require('./../models/todo');
-
-const todos = [{
-  _id: new ObjectID(),
-  text: 'Meet me in Galaxy 3.5'
-},{
-  _id: new ObjectID(),
-  text: 'Can we just meet yesterday?'
-}];
+const {User} = require('./../models/user');
+const {todos, populateTodos, users, populateUsers} = require('./seed/seed');
 
 // beforeEach((done) => {
 //   Todo.remove({}).then(() => done());
 // }); //to empty the db to run the test, as length is equated with 1 at 28.
 
-beforeEach((done) => {
-  Todo.remove({}).then(() => {
-    return Todo.insertMany(todos);
-  }).then(() => done());
-});
+beforeEach(populateUsers);
+beforeEach(populateTodos);
 
 describe('POST /todos', () => { //(type of req Document)
     it('should create a todo item', (done) => { //describe test
@@ -148,20 +139,93 @@ describe('DELETE /todos/:id', () => {
     });
   });
 
-  describe('PATCH /todos/:id', () => {
-    var hexId = todos[0]._id.toHexString();
-    var text = "this should be the new text";
+describe('PATCH /todos/:id', () => {
+  var hexId = todos[0]._id.toHexString();
+  var text = "this should be the new text";
 
-    it('should update the result' , (done) => {
+  it('should update the result' , (done) => {
+    request(app)
+    .patch(`/todos/${hexId}`)
+    .send({
+      text
+    })
+    .expect(200)
+    .expect((res) => {
+      expect(res.body.todo.text).toBe(text);
+    })
+    .end(done);
+  });
+})
+
+describe('GET /users/me', () => {
+  it('should return user if authenticated', (done) => {
       request(app)
-      .patch(`/todos/${hexId}`)
-      .send({
-        text
-      })
-      .expect(200)
-      .expect((res) => {
-        expect(res.body.todo.text).toBe(text);
-      })
+        .get('/users/me')
+        .set('x-auth', users[0].tokens[0].token)//setting headers
+        .expect(200)
+        .expect((res) => {
+          expect(res.body._id).toBe(users[0]._id.toHexString());
+          expect(res.body.email).toBe(users[0].email);
+        })
       .end(done);
-    });
+  });
+  it('should return 401 if not authenticated', (done) => {
+    request(app)
+    .get('/users/me')
+    .expect(401)
+    .expect((res) => {
+      expect(res.body).toEqual({});
+    })
+    .end(done);
   })
+});
+
+describe('POST /users/me', () => {
+  it('should create a new user', (done) => {
+    var email = 'max@max.com';
+    var password = 'maxmax123';
+
+   request(app)
+    .post('/users')
+    .send({email, password})
+    .expect(200)
+    .expect((res) => {
+      expect(res.headers['x-auth']).toExist();
+      expect(res.body._id).toExist();
+      expect(res.body.email).toBe(email);
+    })
+    .end((err) => {
+    if(err) {
+      return done(err);
+    }
+
+    User.findOne({email}).then((user) => {
+      expect(user).toExist();
+      expect(user.password).toNotBe(password);
+      done();
+    })
+  });
+});
+
+it('should return validation error if it is invalid', (done) => {
+    request(app)
+      .post('/users')
+      .send({
+        email: 'and',
+        password: '123'
+      })
+      .expect(400)
+      .end(done)
+  });
+
+// it('should not create user if email in use', (done) => {
+//     request(app)
+//       .post('/users')
+//       .send({
+//         email: users[0].email,
+//         password: 'maxmax123'
+//       })
+//       .expect(400)
+//       .end(done);
+//   }); //This test isnt working, check on it. Although, redundant entries arent allowed in the db.
+});
