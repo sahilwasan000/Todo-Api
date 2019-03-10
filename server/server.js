b023790d8300263d276f5d2c43bf12257e1d7d53
Sahil Wasan <sahilwasan000@gmail.com>
2019-03-11 01:38:36 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -133,6 +133,29 @@ app.get('/users/me', authenticate, (req, res) => {
     res.send(req.user);
 });
 
+//--------Logging In Users-----------//
+
+app.post('/users/login', (req,res) => {
+  var body = _.pick(req.body, ['email', 'password']);
+
+  User.findByCredentials(body.email, body.password).then((user) => {
+    return user.generateAuthToken().then((token) => {
+      res.header('x-auth', token).send(user);
+    });
+  }).catch((e) => {
+    res.status(400).send();
+  });
+});
+
+//-----------Logging Out Users-------------//
+app.delete('/users/me/token', authenticate, (req, res) => {
+  req.user.removeToken(req.token).then(() => {
+    res.status(200).send();
+  }, () => {
+    res.status(400).send();
+  });
+});
+
   module.exports = {app};
 
 
