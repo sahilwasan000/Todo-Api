e5bd044625bced537198881198b0e3e0782180a0
Sahil Wasan <sahilwasan000@gmail.com>
2019-03-10 16:41:27 +0530

    Add files via upload

---
+++ server/tests/seed/seed.js
@@ -0,0 +1,48 @@
+//-----------Seeding Data-------------//
+
+const {ObjectID} = require('mongodb');
+const {Todo} = require('./../../models/todo');
+
+const jwt = require('jsonwebtoken');
+const {User} = require('./../../models/user');
+
+const userOneId = new ObjectID();
+const userTwoId = new ObjectID();
+
+const users = [{
+  _id: userOneId,
+  email: 'sahil@abc.com',
+  password: 'userOnePass',
+  tokens: [{
+    access: 'auth',
+    token: jwt.sign({_id: userOneId, access: 'auth'}, 'abcabc').toString()
+  }]
+}, {
+  _id: userTwoId,
+  email: 'marksdown@gmail.com'
+}];
+
+const todos = [{
+  _id: new ObjectID(),
+  text: 'Meet me in Galaxy 3.5'
+},{
+  _id: new ObjectID(),
+  text: 'Please come fast'
+}];
+
+const populateTodos = (done) => {
+  Todo.remove({}).then(() => {
+    return Todo.insertMany(todos);
+  }).then(() => done());
+};
+
+const populateUsers = (done) => {
+  User.remove({}).then(() => {
+    var userOne = new User(users[0]).save();
+    var userTwo = new User(users[1]).save();
+
+    return Promise.all([userOne, userTwo])
+  }).then(() => done());
+};
+
+module.exports = {todos, populateTodos, users, populateUsers};
