72fe3c347ce52d5a0f5a268d1be544909e00f643
Sahil Wasan <sahilwasan000@gmail.com>
2019-03-12 21:58:25 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -29,9 +29,10 @@ var app =  express();
 
 app.use(bodyParser.json());
 
-app.post('/todos', (req,res) => {
+app.post('/todos', authenticate, (req,res) => {
     var todo = new Todo({
-      text: req.body.text
+      text: req.body.text,
+      _creator: req.user._id
     });
 
     todo.save().then((doc) => {
@@ -41,15 +42,17 @@ app.post('/todos', (req,res) => {
     });
   });
 
-app.get('/todos', (req, res) => {
-    Todo.find().then((todos) => {
+app.get('/todos',authenticate, (req, res) => {
+    Todo.find({
+      _creator: req.user._id
+    }).then((todos) => {
       res.send({todos});
     }, (e) => {
       res.status(400).send(e);
     })
   });
 
-app.get('/todos/:id', (req,res) => {
+app.get('/todos/:id', authenticate, (req,res) => {
     var id = req.params.id;
 
     //res.send(req.params); -> returns key-value pairs for id:data;
@@ -59,7 +62,10 @@ app.get('/todos/:id', (req,res) => {
       return res.status(404).send();
     }
 
-    Todo.findById(id).then((todo) => {
+    Todo.findOne({
+      _id: id,
+      _creator: req.user._id
+    }).then((todo) => {
       if(!todo) {
         return res.status(404).send();
       }
@@ -69,7 +75,7 @@ app.get('/todos/:id', (req,res) => {
       res.status(404).send();
     });
 
-  app.delete('/todos/:id', (req, res) => { //remove id section.
+  app.delete('/todos/:id', authenticate, (req, res) => { //remove id section.
       //get id
       var id = req.params.id;
       //validate id
@@ -77,7 +83,10 @@ app.get('/todos/:id', (req,res) => {
         return res.status(404).send();
       }
       //if we got a valid id, remove it
-      Todo.findByIdAndRemove(id).then((todo) => {
+      Todo.findOneAndRemove({
+        _id: id,
+        _creator: req.user._id
+      }).then((todo) => {
         if(!todo){
           return res.status(404).send();
         }
@@ -89,7 +98,7 @@ app.get('/todos/:id', (req,res) => {
     });
 });
 
- app.patch('/todos/:id', (req,res) => {
+ app.patch('/todos/:id', authenticate, (req,res) => {
    var id = req.params.id;
    var body = _.pick(req.body, ['text']);//pick method to select only those properties which may be edited by the user.
 
@@ -97,7 +106,7 @@ app.get('/todos/:id', (req,res) => {
      return res.status(404).send();
    }
 
-   Todo.findByIdAndUpdate(id, {$set:body}, {new: true}).then((todo) => {
+   Todo.findOneAndUpdate({_id: id, _creator: req.user._id}, {$set:body}, {new: true}).then((todo) => {
      if(!todo){
        return res.status(404).send();
      }
